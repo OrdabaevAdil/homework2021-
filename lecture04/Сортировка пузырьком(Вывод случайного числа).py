85ee89ad77cb2aa642553298fd12f6213f7d623c
Ордабаев Адиль Еркенович <90669241+OrdabaevAdil@users.noreply.github.com>
2021-10-11 11:56:40 +0700

    Add files via upload

---
+++ lecture04/Сортировка пузырьком(Вывод случайного числа).py
@@ -0,0 +1,37 @@
+def bubble_sort(alist):
+    for i in range(len(alist) - 1, 0, -1):
+        no_swap = True
+        for j in range(0, i):
+            if alist[j + 1] < alist[j]:
+                alist[j], alist[j + 1] = alist[j + 1], alist[j]
+                no_swap = False
+        if no_swap:
+            return
+
+import random
+print("Вывод случайного числа при помощи использования random.random()")
+N = input('Введите N=натуральное число - количество повторений')
+
+try:
+    n=int(N)
+except:
+    print('Пожалуйста, введите N=НАТУРАЛЬНОЕ ЧИСЛО!!')
+    input()
+    exit()
+
+k = 1
+a = int(100*random.random())
+b = [a]
+while k<n:
+    a = int(100*random.random())
+    b.append(a)
+    k+=1
+print(b)
+
+alist = b
+alist = [int(x) for x in alist]
+bubble_sort(alist)
+print('Отсортированный список: ', end='')
+print(alist)
+
+input()
