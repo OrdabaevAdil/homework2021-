e38525a9167a704c3b5c940b59a16d98c8b1e046
Ордабаев Адиль Еркенович <90669241+OrdabaevAdil@users.noreply.github.com>
2021-10-23 11:38:12 +0700

    Add files via upload

---
+++ lecture05/Бросок кубиков v1.0.py
@@ -0,0 +1,13 @@
+from random import randint
+
+f = open('D:\PYTHON\WORKS\lecture5\task1.txt')
+from random import randint
+
+f = open('D:\PYTHON\WORKS\lecture5\task1.txt')
+
+
+def Summa():
+    for line in f:
+        print(line)
+Summa()(line)
+Summa()
